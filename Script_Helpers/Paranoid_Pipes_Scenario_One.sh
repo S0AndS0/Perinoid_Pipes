5a633b7abc73bfdac224f6b9c11deb37d53fc05a
S0AndS0 <strangerthanbland@gmail.com>
2016-10-16 21:48:37 +0000

    Formatting & Spelling corrections
    Note the scripts found under this sub-directory are not as well tested as the
    main script of this project, see shellcheck reports for more information on bug
    hunting.

---
+++ Script_Helpers/Paranoid_Pipes_Scenario_One.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+## The following variable should be your encrypted file that has been appended to.
+Var_input_file="${1?No input file to read?}"
+## The following variable should be your named pipe for decrypting
+Var_output_file="${2:-/tmp/out.log}"
+## You may assign the above at run-time using the following example call to
+##  this script: script_name.sh "/path/to/input" "/path/to/output"
+Var_search_output="$3"
+Func_spoon_feed_pipe_decryption(){
+	_input="${@}"
+	_end_of_line='-----END PGP MESSAGE-----'
+	## If input is a file then use standard redirection to mapfile command.
+	##  Else use variable as file redirection trick to get mapfile to build an array from input.
+	if [ -f "${_input}" ]; then
+		mapfile -t _arr_input < "${_input}"
+	else
+		mapfile -t _arr_input <<<"${_input}"
+	fi
+	## Initialize internal count that is either reset or added to in the following loop.
+	let _count=0
+	until [ "${_count}" = "${#_arr_input[@]}" ]; do
+		## If current index in array equals ${_end_of_line} value then append end of line
+		##  to ${_arr_to_parse[@]} and reset ${_arr_input} to include everything not parsed
+		##  and reset the counter. Else we should append the current index to ${_arr_to_parse}
+		##  and loop again until the count and array amount are equal.
+		if [ "${_end_of_line}" = "${_arr_input[${_count}]}" ]; then
+			_arr_to_parse+=( "${_arr_input[${_count}]}" )
+			_arr_input=( "${_arr_input[@]:$((${_count}+1))}" )
+			let _count=0
+		else
+			_arr_to_parse+=( "${_arr_input[${_count}]}" )
+			let _count++
+		fi
+	done
+	unset _count
+	## If above array has some values to parse then start feeding parsing
+	##  function with an array of arrays, one array at a time.
+	if ! [ -z "${_arr_to_parse[@]}" ]; then
+		let _count=0
+		until [ "${_count}" = "${#_arr_to_parse[@]}" ]; do
+			Do_stuff_with_lines "${_arr_to_parse[${_count}]}"
+			let _count++
+		done
+		unset _count
+	fi
+}
+Do_stuff_with_lines(){
+	_enc_input=( "$@" )
+	_decryption_command="$(which gpg) -d"
+	_search_command="$(which grep) -E \"${Var_search_output}\""
+	## If using a named pipe to preform decryption then push encrypted array
+	##  through named pipe's input for use, if output is a file then use
+	##  above decrypting command and append to the file. Else output
+	##  decryption to terminal.
+	if [ -p "${Var_output_file}" ]; then
+		cat <<<"${_enc_input[@]}" > ${Var_output_file}
+	elif [ -f "${Var_output_file}" ]; then
+		if [ -z "${#Var_search_output}" ]; then
+			cat <<<"${_enc_input[@]}" | ${_decryption_command} >> ${Var_output_file}
+		else
+			cat <<<"${_enc_input[@]}" | ${_decryption_command} | ${_search_command} >> ${Var_output_file}
+		fi
+	else
+		if [ -z "${#Var_search_output}" ]; then
+			cat <<<"${_enc_input[@]}" | ${_decryption_command}
+		else
+			cat <<<"${_enc_input[@]}" | ${_decryption_command} | ${_search_command}
+		fi
+	fi
+}
+Main_func(){
+	Func_spoon_feed_pipe_decryption "${Var_input_file}"
+}
+Main_func
---
+++ Script_Helpers/ReadMe.md
@@ -0,0 +1,47 @@
+# Warning
+
+ > The scripts found under this directory are significantly less tested or
+ completely un-tested! Be sure to run `shellcheck <script_name>` against any
+ script found within this directory prior to executing it.
+
+## Scripts available within `Script_Helpers/` directory
+
+[Paranoid_Pipes_Scenario_One.sh](Paranoid_Pipes_Scenario_One.sh)
+ This script is referenced within [../Documentation/Paranoid_Pipes_Scenario_One.md](../Documentation/Paranoid_Pipes_Scenario_One.md)
+ as an example of using customized named pipe listener scripts to decrypt log
+ files that have appended encrypted contents. This is indented as a *work-around*
+ for limitations of GnuPG not *seeming to* recognize listed encrypted data beyond
+ the first entry.
+
+## Licensing notice for scripts/code examples
+
+```
+Paranoid_Pipes.sh, maker of named pipe parsing template Bash scripts.
+ Copyright (C) 2016 S0AndS0
+This program is free software: you can redistribute it and/or modify it
+ under the terms of the GNU Affero General Public License as published by
+ the Free Software Foundation, version 3 of the License. You should have
+ received a copy of the GNU Afferno General Public License along with this
+ program. If not, see <http://www.gnu.org/licenses/>. Contact authors of
+ [Paranoid_Pipes.sh] at: strangerthanbland@gmail.com
+```
+
+[Link to related license](GNU_AGPLv3_Code.md)
+
+ > See [`..Licenses/ReadMe.md`](..Licenses/ReadMe.md) for further information as
+ to the hows and whys of these licensing choices made by this project's authors.
+
+## Licensing notice for this file
+
+```
+    Copyright (C) 2016 S0AndS0.
+    Permission is granted to copy, distribute and/or modify this document under
+    the terms of the GNU Free Documentation License, Version 1.3 published by
+    the Free Software Foundation; with the Invariant Sections being
+    "Title page". A copy of the license is included in the directory entitled
+    "License".
+```
+
+[Link to title page](Contributing_Financially.md)
+
+[Link to related license](../Licenses/GNU_FDLv1.3_Documentation.md)
