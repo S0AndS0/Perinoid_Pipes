48a962c91cd0f04da4b9c830521f3abee74cf0ce
S0AndS0 <strangerthanbland@gmail.com>
2016-10-17 19:42:35 +0000

    Shellcheck corrections: note still less tested than main script
    Read the guides under the documentation directory that have similar names as
    those found as scripts under this directory for understanding of how these are
    to be used. Note these are currently not tested but also not popping any
    critical bug reports on the authors local checks with shellcheck.

--- Script_Helpers/Paranoid_Pipes_Scenario_One.sh
+++ Script_Helpers/Paranoid_Pipes_Scenario_One.sh
@@ -7,25 +7,28 @@ Var_output_file="${2:-/tmp/out.log}"
 ##  this script: script_name.sh "/path/to/input" "/path/to/output"
 Var_search_output="$3"
 Func_spoon_feed_pipe_decryption(){
-	_input="${@}"
+	_input=( "${@}" )
 	_end_of_line='-----END PGP MESSAGE-----'
 	## If input is a file then use standard redirection to mapfile command.
 	##  Else use variable as file redirection trick to get mapfile to build an array from input.
-	if [ -f "${_input}" ]; then
-		mapfile -t _arr_input < "${_input}"
+	if [ -f "${_input[@]}" ]; then
+		mapfile -t _arr_input < "${_input[@]}"
 	else
-		mapfile -t _arr_input <<<"${_input}"
+		mapfile -t _arr_input <<<"${_input[@]}"
 	fi
 	## Initialize internal count that is either reset or added to in the following loop.
 	let _count=0
 	until [ "${_count}" = "${#_arr_input[@]}" ]; do
-		## If current index in array equals ${_end_of_line} value then append end of line
-		##  to ${_arr_to_parse[@]} and reset ${_arr_input} to include everything not parsed
-		##  and reset the counter. Else we should append the current index to ${_arr_to_parse}
-		##  and loop again until the count and array amount are equal.
+		## If current index in array equals ${_end_of_line} value then
+		##  append end of line to ${_arr_to_parse[@]} and reset
+		##  ${_arr_input} to include everything not parsed and reset the
+		##  counter. Else we should append the current index to
+		##  ${_arr_to_parse} and loop again until the count and array
+		##  amount are equal.
 		if [ "${_end_of_line}" = "${_arr_input[${_count}]}" ]; then
 			_arr_to_parse+=( "${_arr_input[${_count}]}" )
-			_arr_input=( "${_arr_input[@]:$((${_count}+1))}" )
+			let _count++
+			_arr_input=( "${_arr_input[@]:${_count}}" )
 			let _count=0
 		else
 			_arr_to_parse+=( "${_arr_input[${_count}]}" )
@@ -35,7 +38,7 @@ Func_spoon_feed_pipe_decryption(){
 	unset _count
 	## If above array has some values to parse then start feeding parsing
 	##  function with an array of arrays, one array at a time.
-	if ! [ -z "${_arr_to_parse[@]}" ]; then
+	if [ -n "${_arr_to_parse[@]}" ]; then
 		let _count=0
 		until [ "${_count}" = "${#_arr_to_parse[@]}" ]; do
 			Do_stuff_with_lines "${_arr_to_parse[${_count}]}"
@@ -46,25 +49,23 @@ Func_spoon_feed_pipe_decryption(){
 }
 Do_stuff_with_lines(){
 	_enc_input=( "$@" )
-	_decryption_command="$(which gpg) -d"
-	_search_command="$(which grep) -E \"${Var_search_output}\""
 	## If using a named pipe to preform decryption then push encrypted array
 	##  through named pipe's input for use, if output is a file then use
 	##  above decrypting command and append to the file. Else output
 	##  decryption to terminal.
 	if [ -p "${Var_output_file}" ]; then
-		cat <<<"${_enc_input[@]}" > ${Var_output_file}
+		cat <<<"${_enc_input[@]}" > "${Var_output_file}"
 	elif [ -f "${Var_output_file}" ]; then
-		if [ -z "${#Var_search_output}" ]; then
-			cat <<<"${_enc_input[@]}" | ${_decryption_command} >> ${Var_output_file}
+		if [ -z "${Var_search_output}" ]; then
+			cat <<<"${_enc_input[@]}" | gpg -d >> "${Var_output_file}"
 		else
-			cat <<<"${_enc_input[@]}" | ${_decryption_command} | ${_search_command} >> ${Var_output_file}
+			cat <<<"${_enc_input[@]}" | gpg -d | grep -E "${Var_search_output}" >> "${Var_output_file}"
 		fi
 	else
 		if [ -z "${#Var_search_output}" ]; then
-			cat <<<"${_enc_input[@]}" | ${_decryption_command}
+			cat <<<"${_enc_input[@]}" | gpg -d
 		else
-			cat <<<"${_enc_input[@]}" | ${_decryption_command} | ${_search_command}
+			cat <<<"${_enc_input[@]}" | gpg -d | grep -E "${Var_search_output}"
 		fi
 	fi
 }
