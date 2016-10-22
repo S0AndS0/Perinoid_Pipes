e6af1ca760eed8bbc0d8369cdaae814d16349fcd
S0AndS0 <strangerthanbland@gmail.com>
2016-10-22 04:13:19 +0000

    Well looks like somethings are still needed on the main script too

--- .travis-ci/lib/config_pipe_variables_encrypt.sh
+++ .travis-ci/lib/config_pipe_variables_encrypt.sh
@@ -4,17 +4,17 @@ Arr_encrypt_opts+=( ---Var_script_copy_save="yes" )
 #export Var_script_copy_save="yes"
 Arr_encrypt_opts+=( ---Var_pipe_quit_string="quit" )
 #export Var_pipe_quit_string="quit"
-Arr_encrypt_opts+=( ---Var_script_copy_name="/tmp/Paranoid_Pipes/encrypt.sh" )
+Arr_encrypt_opts+=( ---Var_script_copy_name="/tmp/encrypt.sh" )
 #export Var_script_copy_name="/tmp/Paranoid_Pipes/encrypt.sh"
-Arr_encrypt_opts+=( ---Var_log_file_name="/tmp/Paranoid_Pipes/encrypt.log" )
-#export Var_log_file_name="/tmp/Paranoid_Pipes/encrypt.log"
-Arr_encrypt_opts+=( ---Var_pipe_file_name="/tmp/Paranoid_Pipes/encrypt.pipe" )
-#export Var_pipe_file_name="/tmp/Paranoid_Pipes/encrypt.pipe"
-Arr_encrypt_opts+=( ---Var_parsing_output_file="/tmp/Paranoid_Pipes/encrypt.gpg" )
-#export Var_parsing_output_file="/tmp/Paranoid_Pipes/encrypt.gpg"
+Arr_encrypt_opts+=( ---Var_log_file_name="/tmp/encrypt.log" )
+#export Var_log_file_name="/tmp/encrypt.log"
+Arr_encrypt_opts+=( ---Var_pipe_file_name="/tmp/encrypt.pipe" )
+#export Var_pipe_file_name="/tmp/encrypt.pipe"
+Arr_encrypt_opts+=( ---Var_parsing_output_file="/tmp/encrypt.gpg" )
+#export Var_parsing_output_file="/tmp/encrypt.gpg"
 Arr_encrypt_opts+=( ---Var_gpg_recipient="${Var_gnupg_email}" )
 #export Var_gpg_recipient="${Var_gnupg_email}"
 Arr_encrypt_opts+=( ---Var_log_rotate_recipient="${Var_gnupg_email}" )
 #export Var_log_rotate_recipient="${Var_gnupg_email}"
-Arr_encrypt_opts+=( ---Var_parsing_bulk_out_dir="/tmp/Paranoid_Pipes" )
-#export Var_parsing_bulk_out_dir="/tmp/Paranoid_Pipes"
+Arr_encrypt_opts+=( ---Var_parsing_bulk_out_dir="/tmp" )
+#export Var_parsing_bulk_out_dir="/tmp"
