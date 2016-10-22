f94cecc5a96ddcb5393c19933880fa7fb451316c
S0AndS0 <strangerthanbland@gmail.com>
2016-10-22 22:30:03 +0000

    Hmm... more read/write issues from Travis-CI?

--- .travis-ci/script_encrypt.sh
+++ .travis-ci/script_encrypt.sh
@@ -21,10 +21,12 @@ fi
 if [ -p "${Var_encrypt_pipe_location}" ]; then
 	_test_string=$(base64 /dev/urandom | tr -cd 'a-zA-Z0-9' | head -c"${Var_pass_length}")
 	Func_run_sanely "echo \"${_test_string}\" > \"${Var_encrypt_pipe_location}\"" "${USER}"
-	echo "# ${Var_script_name} showing preview of encrypted data #"
-	Func_run_sanely "cat \"${Var_encrypted_location}\"" "${USER}"
-	echo "# ${Var_script_name} testing decryption of file"
-	Func_run_sanely "gpg --batch --yes --decrypt ${Var_encrypted_location} --passphrase-file ${Var_pass_location}"
+	if [ -r "${Var_encrypted_location}" ]; then
+		echo "# ${Var_script_name} showing preview of encrypted data #"
+		Func_run_sanely "cat ${Var_encrypted_location}" "${USER}"
+		echo "# ${Var_script_name} testing decryption of file"
+		Func_run_sanely "gpg --batch --yes --decrypt ${Var_encrypted_location} --passphrase-file ${Var_pass_location}"
+	fi
 	Func_run_sanely "echo \"quit\" > \"${Var_encrypt_pipe_location}\"" "${USER}"
 else
 	echo "# ${Var_script_name} could not find: ${Var_encrypt_pipe_location}"
