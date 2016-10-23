379eda029cba93517769e9a8607d7dd2c57431f7
S0AndS0 <strangerthanbland@gmail.com>
2016-10-23 20:35:45 +0000

    Fixing up tests

--- .travis-ci/before_install_test_key.sh
+++ .travis-ci/before_install_test_key.sh
@@ -5,11 +5,11 @@ export Var_script_name="${0##*/}"
 source "${Var_script_dir}/lib/functions.sh"
 Func_source_file "${Var_script_dir}/lib/variables.sh"
 _test_string=$(base64 /dev/urandom | tr -cd 'a-zA-Z0-9' | head -c"${Var_pass_length}")
-_test_encryption_opts="--recipient ${Var_gnupg_email} --encrypt --output ${Var_test_gpg_location}"
-_test_decryption_opts="--passphrase-file ${Var_pass_location} --decrypt ${Var_test_gpg_location} --output ${Var_test_raw_location}"
+_test_encryption_opts="--recipient ${Var_gnupg_email} --encrypt"
+_test_decryption_opts="--passphrase-file ${Var_pass_location} --decrypt ${Var_test_gpg_location}"
 ## Try encrypting text to new key
-Func_run_sanely "echo ${_test_string} | gpg ${_test_encryption_opts}" "${USER}"
-Func_run_sanely "gpg ${_test_decryption_opts}" "${USER}"
+Func_run_sanely "cat <<<${_test_string} | gpg ${_test_encryption_opts} >> ${Var_test_gpg_location}" "${USER}"
+Func_run_sanely "gpg ${_test_decryption_opts} >> ${Var_test_raw_location}" "${USER}"
 _test_string_decrypted=$(cat ${Var_test_raw_location})
 if [[ "${_test_string}" == "${_test_string_decrypted}" ]]; then
 	echo "# ${Var_script_name} reports all checks: OK"
