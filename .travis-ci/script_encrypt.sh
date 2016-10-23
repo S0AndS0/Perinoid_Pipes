364706555ec4be9f1d805848ee4d22516fb1f8a9
S0AndS0 <strangerthanbland@gmail.com>
2016-10-23 17:31:20 +0000

    Testing build scripts under sudo user permissions.

--- .travis-ci/script_encrypt.sh
+++ .travis-ci/script_encrypt.sh
@@ -7,12 +7,13 @@ Func_source_file "${Var_script_dir}/lib/variables.sh"
 Func_source_file "${Var_script_dir}/lib/config_pipe_variables_encrypt.sh"
 ## Generate temp-key pare for testing encryption with public key operations.
 _pass_phrase=$(base64 /dev/urandom | tr -cd 'a-zA-Z0-9' | head -c"${Var_pass_length}")
-Func_run_sanely "echo ${_pass_phrase} > ${Var_pass_location}" "${USER}"
-Func_run_sanely "Func_gen_gnupg_test_keys ${_pass_phrase}" "${USER}"
+Func_run_sanely "echo ${_pass_phrase} > ${Var_pass_location}" "0"
+Func_run_sanely "Func_gen_gnupg_test_keys ${_pass_phrase}" "0"
 ## If installed script is executable then make test keys,
 ## pipe and listener, else exit wth errors
 if [ -e "${Var_install_path}/${Var_install_name}" ]; then
-	Func_run_sanely "${Var_install_path}/${Var_install_name} ${Arr_encrypt_opts[*]}" "${USER}"
+	_remove_new_lines=$(echo -n "${Arr_encrypt_opts[*]}")
+	Func_run_sanely "${Var_install_path}/${Var_install_name} ${_remove_new_lines}" "0"
 #	Func_run_sanely "${Var_install_path}/${Var_install_name} ${Arr_encrypt_opts[*]}" "0"
 else
 	echo "# ${Var_script_name} could not find: ${Var_install_path}/${Var_install_name}"
@@ -21,10 +22,10 @@ fi
 ## If test pipe file exists then test, else exit with errors
 if [ -p "${Var_encrypt_pipe_location}" ]; then
 	_test_string=$(base64 /dev/urandom | tr -cd 'a-zA-Z0-9' | head -c"${Var_pass_length}")
-	Func_run_sanely "echo ${_test_string} > ${Var_encrypt_pipe_location}" "${USER}"
+	Func_run_sanely "echo ${_test_string} > ${Var_encrypt_pipe_location}" "0"
 	if [ -r "${Var_encrypted_location}" ]; then
-		Func_run_sanely "cat ${Var_encrypted_location}" "${USER}"
-		Func_run_sanely "gpg --batch --yes --decrypt ${Var_encrypted_location} --passphrase-file ${Var_pass_location}" "${USER}"
+		Func_run_sanely "cat ${Var_encrypted_location}" "0"
+		Func_run_sanely "gpg --batch --yes --decrypt ${Var_encrypted_location} --passphrase-file ${Var_pass_location}" "0"
 	else
 		echo "# ${Var_script_name} could not find: ${Var_encrypted_location}"
 		if [ -f "${Var_encrypted_location}" ]; then
@@ -33,7 +34,7 @@ if [ -p "${Var_encrypt_pipe_location}" ]; then
 			echo "# ${Var_script_name} reports it not a file: ${Var_encrypted_location}"
 		fi
 	fi
-	Func_run_sanely "echo quit > ${Var_encrypt_pipe_location}" "${USER}"
+	Func_run_sanely "echo quit > ${Var_encrypt_pipe_location}" "0"
 else
 	echo "# ${Var_script_name} could not find: ${Var_encrypt_pipe_location}"
 	exit 1
