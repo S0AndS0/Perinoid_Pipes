78d266584ef5a541d2441cdbd05693421e5dddbd
S0AndS0 <strangerthanbland@gmail.com>
2016-10-23 20:54:57 +0000

    Fighting with GnuPG now
    Now that authors have a handle on how Travis-CI handles things the new
    bug-stomping experience is with GnuPG and scripted encryption and
    decryption operations.

--- .travis-ci/before_install_test_key.sh
+++ .travis-ci/before_install_test_key.sh
@@ -6,10 +6,14 @@ source "${Var_script_dir}/lib/functions.sh"
 Func_source_file "${Var_script_dir}/lib/variables.sh"
 _test_string=$(base64 /dev/urandom | tr -cd 'a-zA-Z0-9' | head -c"${Var_pass_length}")
 _test_encryption_opts="--recipient ${Var_gnupg_email} --encrypt"
-_test_decryption_opts="--passphrase-file ${Var_pass_location} --decrypt ${Var_test_gpg_location}"
+_test_decryption_opts="--yes --batch --passphrase-file ${Var_pass_location} --decrypt ${Var_test_gpg_location}"
 ## Try encrypting text to new key
-Func_run_sanely "echo ${_test_string} | gpg ${_test_encryption_opts} >> ${Var_test_gpg_location}" "${USER}"
-Func_run_sanely "gpg ${_test_decryption_opts} >> ${Var_test_raw_location}" "${USER}"
+Func_run_sanely "echo \"${_test_string}\" | gpg ${_test_encryption_opts} >> ${Var_test_gpg_location}" "${USER}"
+if [ -f "${Var_test_gpg_location}" ]; then
+	Func_run_sanely "gpg ${_test_decryption_opts} >> ${Var_test_raw_location}" "${USER}"
+else
+	echo "# ${Var_script_name} cannot find: ${Var_test_gpg_location}"
+fi
 _test_string_decrypted=$(cat ${Var_test_raw_location})
 if [[ "${_test_string}" == "${_test_string_decrypted}" ]]; then
 	echo "# ${Var_script_name} reports all checks: OK"
