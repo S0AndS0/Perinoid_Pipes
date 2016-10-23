b12bf709ef5dfa3086b3edfd6de5858ad25347a9
S0AndS0 <strangerthanbland@gmail.com>
2016-10-23 22:16:00 +0000

    Trying alternet methods with GnuPG

--- .travis-ci/before_install_test_key.sh
+++ .travis-ci/before_install_test_key.sh
@@ -6,14 +6,14 @@ source "${Var_script_dir}/lib/functions.sh"
 Func_source_file "${Var_script_dir}/lib/variables.sh"
 _test_string=$(base64 /dev/urandom | tr -cd 'a-zA-Z0-9' | head -c"${Var_pass_length}")
 _test_encryption_opts="--recipient ${Var_gnupg_email} --encrypt"
-_test_decryption_opts="--always-trust --passphrase-file ${Var_pass_location} --decrypt ${Var_test_gpg_location}"
+_test_decryption_opts="--always-trust --passphrase-fd 0 --decrypt ${Var_test_gpg_location}"
 ## Try encrypting text to new key
 cat <<<"${_test_string}" | gpg ${_test_encryption_opts} >> ${Var_test_gpg_location}
 #Func_run_sanely "echo \"${_test_string}\" | gpg ${_test_encryption_opts} >> ${Var_test_gpg_location}" "${USER}"
 _exit_status=$?
 Func_check_exit_status "${_exit_status}"
 if [ -f "${Var_test_gpg_location}" ]; then
-	gpg ${_test_decryption_opts} >> ${Var_test_raw_location}
+	cat  ${Var_pass_location} | gpg ${_test_decryption_opts} >> ${Var_test_raw_location}
 #	Func_run_sanely "gpg ${_test_decryption_opts} >> ${Var_test_raw_location}" "${USER}"
 	_exit_status=$?
 	Func_check_exit_status "${_exit_status}"
