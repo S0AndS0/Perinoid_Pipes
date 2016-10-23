1a83a5668764a8341e416782a1330d99ee54e1f6
S0AndS0 <strangerthanbland@gmail.com>
2016-10-23 20:17:06 +0000

    Well redirection did not work
    Trying the piping methods that usually work with GnuPG

--- .travis-ci/before_install_test_key.sh
+++ .travis-ci/before_install_test_key.sh
@@ -5,14 +5,10 @@ export Var_script_name="${0##*/}"
 source "${Var_script_dir}/lib/functions.sh"
 Func_source_file "${Var_script_dir}/lib/variables.sh"
 _test_string=$(base64 /dev/urandom | tr -cd 'a-zA-Z0-9' | head -c"${Var_pass_length}")
-_test_encryption_opts="--recipient ${Var_gnupg_email} --encrypt <<<${_test_string} --output ${Var_test_gpg_location}"
+_test_encryption_opts="--recipient ${Var_gnupg_email} --encrypt --output ${Var_test_gpg_location}"
 _test_decryption_opts="--passphrase-file ${Var_pass_location} --decrypt ${Var_test_gpg_location} --output ${Var_test_raw_location}"
-# ${Var_gnupg_email}
-# ${Var_test_gpg_location}
-# ${Var_test_raw_location}
-# ${Var_pass_location}
 ## Try encrypting text to new key
-Func_run_sanely "gpg ${_test_encryption_opts}" "${USER}"
+Func_run_sanely "echo ${_test_string} | gpg ${_test_encryption_opts}" "${USER}"
 Func_run_sanely "gpg ${_test_decryption_opts}" "${USER}"
 _test_string_decrypted=$(cat ${Var_test_raw_location})
 if [[ "${_test_string}" == "${_test_string_decrypted}" ]]; then
