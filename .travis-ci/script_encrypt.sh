f7f9dc87e62d8b918fc6fda318dd100ba4290912
S0AndS0 <strangerthanbland@gmail.com>
2016-10-23 01:12:07 +0000

    More tests for Travis-CI
    These commited out portions should cause the main script to be tested
    instead of its writen copies.

--- .travis-ci/script_encrypt.sh
+++ .travis-ci/script_encrypt.sh
@@ -7,12 +7,13 @@ Func_source_file "${Var_script_dir}/lib/variables.sh"
 Func_source_file "${Var_script_dir}/lib/config_pipe_variables_encrypt.sh"
 ## Generate temp-key pare for testing encryption with public key operations.
 _pass_phrase=$(base64 /dev/urandom | tr -cd 'a-zA-Z0-9' | head -c"${Var_pass_length}")
-echo "${_pass_phrase}" > "${Var_pass_location}"
+Func_run_sanely "echo ${_pass_phrase} > ${Var_pass_location}" "${USER}"
 Func_run_sanely "Func_gen_gnupg_test_keys ${_pass_phrase}" "${USER}"
 ## If installed script is executable then make test keys,
 ## pipe and listener, else exit wth errors
 if [ -e "${Var_install_path}/${Var_install_name}" ]; then
-	Func_run_sanely "${Var_install_path}/${Var_install_name} ${Arr_encrypt_opts[*]}" "0"
+	Func_run_sanely "${Var_install_path}/${Var_install_name} ${Arr_encrypt_opts[*]}" "${USER}"
+#	Func_run_sanely "${Var_install_path}/${Var_install_name} ${Arr_encrypt_opts[*]}" "0"
 else
 	echo "# ${Var_script_name} could not find: ${Var_install_path}/${Var_install_name}"
 	exit 1
@@ -22,10 +23,8 @@ if [ -p "${Var_encrypt_pipe_location}" ]; then
 	_test_string=$(base64 /dev/urandom | tr -cd 'a-zA-Z0-9' | head -c"${Var_pass_length}")
 	Func_run_sanely "echo ${_test_string} > ${Var_encrypt_pipe_location}" "${USER}"
 	if [ -r "${Var_encrypted_location}" ]; then
-		echo "# ${Var_script_name} showing preview of encrypted data #"
 		Func_run_sanely "cat ${Var_encrypted_location}" "${USER}"
-		echo "# ${Var_script_name} testing decryption of file"
-		Func_run_sanely "gpg --batch --yes --decrypt ${Var_encrypted_location} --passphrase-file ${Var_pass_location}"
+		Func_run_sanely "gpg --batch --yes --decrypt ${Var_encrypted_location} --passphrase-file ${Var_pass_location}" "${USER}"
 	else
 		echo "# ${Var_script_name} could not find: ${Var_encrypted_location}"
 		if [ -f "${Var_encrypted_location}" ]; then
