d83cd0d292cc0c6ccc6b576237ca7a8d75c9c5e1
S0AndS0 <strangerthanbland@gmail.com>
2016-10-30 21:08:09 +0000

    More attempts at gen-revoke
    Also added command line options for exporting private key to a file for
    auto-builds such that this process is also being tested during builds.

--- .travis-ci/before_script_gen_key.sh
+++ .travis-ci/before_script_gen_key.sh
@@ -8,5 +8,5 @@ echo "# ${Var_script_name} started at: $(date -u +%s)"
 echo "# ${Var_script_name} running: chmod u+x Script_Helpers/GnuPG_Gen_Key.sh"
 chmod u+x Script_Helpers/GnuPG_Gen_Key.sh
 echo "# ${Var_script_name} running: Script_Helpers/GnuPG_Gen_Key.sh --prompt-for-pass-yn='no' --auto-pass-length=\"${Var_pass_length}\" --save-pass-yn='yes' --save-pass-location=\"${Var_pass_location}\" --gnupg-email=\"${Var_gnupg_email}\""
-Script_Helpers/GnuPG_Gen_Key.sh --prompt-for-pass-yn='no' --auto-pass-length="${Var_pass_length}" --save-pass-yn='yes' --save-pass-location="${Var_pass_location}" --gnupg-email="${Var_gnupg_email}"
+Script_Helpers/GnuPG_Gen_Key.sh --prompt-for-pass-yn='no' --auto-pass-length="${Var_pass_length}" --save-pass-yn='yes' --save-pass-location="${Var_pass_location}" --gnupg-email="${Var_gnupg_email}" --gnupg-export-private-key-yn='yes'
 echo "# ${Var_script_name} finished at: $(date -u +%s)"
