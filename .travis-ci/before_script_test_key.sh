a2ce4f5f2961c74b02834de225042dba5bec1552
S0AndS0 <strangerthanbland@gmail.com>
2016-11-04 06:39:39 +0000

    Sweet, looks like others have tried similar
    Gota like blogers for tossing up solutions to these very spicific usage
    cases for GnuPG, supper cool if this worked.

--- .travis-ci/before_script_test_key.sh
+++ .travis-ci/before_script_test_key.sh
@@ -6,8 +6,8 @@ source "${Var_script_dir}/lib/functions.sh"
 Func_source_file "${Var_script_dir}/lib/variables.sh"
 _test_string=$(base64 /dev/urandom | tr -cd 'a-zA-Z0-9' | head -c"${Var_pass_length}")
 _test_encryption_opts="--recipient ${Var_gnupg_email} --encrypt"
-exec 3<${Var_pass_location}
-_test_decryption_opts="--always-trust --passphrase-fd 3 --decrypt"
+#exec 3<${Var_pass_location}
+_test_decryption_opts="--always-trust --passphrase-fd 3 -c 3< <(cat ${Var_pass_location}) --decrypt"
 #_test_decryption_opts="--always-trust --passphrase-fd 0 --decrypt ${Var_test_gpg_location}"
 echo "# ${Var_script_name} started at: $(date -u +%s)"
 ## Try encrypting text to new key
--- .travis-ci/lib/variables.sh
+++ .travis-ci/lib/variables.sh
@@ -26,6 +26,7 @@ export Var_decrypt_raw_location="${PWD}/decrypt_one.txt"
 ##  Note that this script also re-uses the 'Var_raw_test_location' variable.
 export Var_decrypt_pipe_location="${PWD}/decrypt.pipe"
 export Var_decrypted_location="${Var_decrypt_pipe_location%.*}.txt"
-export Var_gnupg_decrypt_opts="--always-trust --passphrase-fd 3 --decrypt"
-#export Var_gnupg_decrypt_opts="--batch --yes --no-tty --always-trust --passphrase-file ${Var_pass_location} --decrypt"
+## Found redirection trick at: https://possiblelossofprecision.net/?p=413
+export Var_gnupg_decrypt_opts="--always-trust --passphrase-fd 3 -c 3< <(cat ${Var_pass_location}) --decrypt"
+#export Var_gnupg_decrypt_opts="--batch --yes --no-tty --always-trust --passphrase-fd 3 -c 3< <(cat ${Var_pass_location}) --decrypt"
 #export Var_gnupg_decrypt_opts="--always-trust --passphrase-fd 0 <(${Var_pass_location}) --decrypt"
