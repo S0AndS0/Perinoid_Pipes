64650f67c4bd9fa496b1e6ffc2e08ee426ef0d47
S0AndS0 <strangerthanbland@gmail.com>
2016-11-07 17:34:38 +0000

    Cosmetic changes
    Moved 'exec' usage as well as anounced when the passphrase file is assigned
    & unassigned to a file descriptor... May have to re-edit the equivantcy
    checks though.

--- .travis-ci/before_script_test_key.sh
+++ .travis-ci/before_script_test_key.sh
@@ -6,7 +6,6 @@ source "${Var_script_dir}/lib/functions.sh"
 Func_source_file "${Var_script_dir}/lib/variables.sh"
 _test_string=$(base64 /dev/urandom | tr -cd 'a-zA-Z0-9' | head -c"${Var_pass_length}")
 _test_encryption_opts="--recipient ${Var_gnupg_email} --encrypt"
-exec 9<${Var_pass_location}
 echo "# ${Var_script_name} started at: $(date -u +%s)"
 ## Try encrypting text to new key
 echo "# ${Var_script_name} running: cat <<<\"${_test_string}\" | gpg ${_test_encryption_opts} >> ${Var_test_gpg_location}"
@@ -14,10 +13,14 @@ cat <<<"${_test_string}" | gpg ${_test_encryption_opts} >> ${Var_test_gpg_locati
 _exit_status=$?
 Func_check_exit_status "${_exit_status}"
 if [ -f "${Var_test_gpg_location}" ]; then
+	echo "# ${Var_script_name} running: exec 9<\"${Var_pass_location}\""
+	exec 9<"${Var_pass_location}"
 	echo "# ${Var_script_name} running: cat ${Var_test_gpg_location} | gpg ${Var_gnupg_decrypt_opts} >> ${Var_test_raw_location}"
 	cat ${Var_test_gpg_location} | gpg ${Var_gnupg_decrypt_opts} >> ${Var_test_raw_location}
 	_exit_status=$?
 	Func_check_exit_status "${_exit_status}"
+	echo "# ${Var_script_name} running: exec 9>&-"
+	exec 9>&-
 else
 	echo "# ${Var_script_name} cannot find: ${Var_test_gpg_location}"
 fi
@@ -27,6 +30,6 @@ if [[ "${_test_string}" == "${_test_string_decrypted}" ]]; then
 	echo "# ${Var_script_name}: [${_test_string}] = [${_test_string_decrypted}]"
 else
 	echo "# ${Var_script_name} reports all checks: Not ok"
-	echo "# ${Var_script_name} could not finde equivalance between: [${_test_string}] & [${_test_string_decrypted}]"
+	echo "# ${Var_script_name} could not find equivalance between: [${_test_string}] & [${_test_string_decrypted}]"
 fi
 echo "# ${Var_script_name} finished at: $(date -u +%s)"
