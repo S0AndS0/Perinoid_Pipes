6f8183598940f96973e3d282c48edb2991dbeb42
S0AndS0 <strangerthanbland@gmail.com>
2016-11-10 22:39:10 +0000

    Looks like a missing space

--- .travis-ci/before_script_gen_key.sh
+++ .travis-ci/before_script_gen_key.sh
@@ -7,6 +7,6 @@ Func_source_file "${Var_script_dir}/lib/variables.sh"
 echo "# ${Var_script_name} started at: $(date -u +%s)"
 echo "# ${Var_script_name} running: chmod u+x Script_Helpers/GnuPG_Gen_Key.sh"
 chmod u+x Script_Helpers/GnuPG_Gen_Key.sh
-echo "# ${Var_script_name} running: Script_Helpers/GnuPG_Gen_Key.sh --prompt-for-pass-yn='no' --auto-pass-length=\"${Var_pass_length}\" --save-pass-yn='yes' --save-pass-location=\"${Var_pass_location}\" --gnupg-conf-save-yn='yes'--gnupg-email=\"${Var_gnupg_email}\" --gnupg-export-private-key-yn='yes'"
-Script_Helpers/GnuPG_Gen_Key.sh --prompt-for-pass-yn='no' --auto-pass-length="${Var_pass_length}" --save-pass-yn='yes' --save-pass-location="${Var_pass_location}" --gnupg-conf-save-yn='yes'--gnupg-email="${Var_gnupg_email}" --gnupg-export-private-key-yn='yes'
+echo "# ${Var_script_name} running: Script_Helpers/GnuPG_Gen_Key.sh --prompt-for-pass-yn='no' --auto-pass-length=\"${Var_pass_length}\" --save-pass-yn='yes' --save-pass-location=\"${Var_pass_location}\" --gnupg-conf-save-yn='yes' --gnupg-email=\"${Var_gnupg_email}\" --gnupg-export-private-key-yn='yes'"
+Script_Helpers/GnuPG_Gen_Key.sh --prompt-for-pass-yn='no' --auto-pass-length="${Var_pass_length}" --save-pass-yn='yes' --save-pass-location="${Var_pass_location}" --gnupg-conf-save-yn='yes' --gnupg-email="${Var_gnupg_email}" --gnupg-export-private-key-yn='yes'
 echo "# ${Var_script_name} finished at: $(date -u +%s)"
