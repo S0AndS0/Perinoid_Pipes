4e5209d79d22ec332a54ebc99aa9fcf73dccbce0
S0AndS0 <strangerthanbland@gmail.com>
2016-11-18 23:08:51 +0000

    Re-ordering was the correct choice
    Authors make mistakes sometimes, in this case we where pushing a file path
    that was not a file just yet. Re-enabled double quotes around file checks
    within main script.

--- Paranoid_Pipes.sh
+++ Paranoid_Pipes.sh
@@ -1023,8 +1023,8 @@ Func_mkpipe_reader(){
 			case "${Var_save_encryption_yn}" in
 				y|Y|yes|Yes)
 					## Test if input is a file path otherwise push it through parsing command
-					if [ -f ${_mapped_array} ]; then
-#					if [ -f "${_mapped_array}" ]; then
+#					if [ -f ${_mapped_array} ]; then
+					if [ -f "${_mapped_array}" ]; then
 						## Check for bulk output directory to save encrypted files to, make one
 						##  if nonexistent
 						if ! [ -d "${Var_parsing_bulk_out_dir}" ]; then
