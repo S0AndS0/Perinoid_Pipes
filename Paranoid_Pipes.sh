126d432854fca5d0847d25d1b8fe189f71e95353
S0AndS0 <strangerthanbland@gmail.com>
2016-11-20 22:31:40 +0000

    Fixed 'for' loop?
    Also added regonition for alternet form of 'tar' decompression and commands
    within main script to try thier use... this may fail.

--- Paranoid_Pipes.sh
+++ Paranoid_Pipes.sh
@@ -1038,9 +1038,9 @@ Func_mkpipe_reader(){
 							${Var_mkdir_exec_path} -vp "${Var_parsing_bulk_out_dir}"
 						fi
 						Var_star_date="$(date -u +%s)"
-						#${Var_cat_exec_path} <<<"${_mapped_array}" | ${Var_tar_exec_path} cz | ${Var_parsing_command} > ${Var_parsing_bulk_out_dir}/${Var_star_date}_dir.tgz.gpg
-						gpg-zip --output ${Var_parsing_bulk_out_dir}/${Var_star_date}_dir.tar.gpg --encrypt --recipient ${Var_gpg_recipient} ${_mapped_array}
+						#gpg-zip --output ${Var_parsing_bulk_out_dir}/${Var_star_date}_dir.tar.gpg --encrypt --recipient ${Var_gpg_recipient} ${_mapped_array}
 						## Either of above works for encrypting directories now.
+						${Var_tar_exec_path} -cz - "${_mapped_array}" | ${Var_parsing_command} > ${Var_parsing_bulk_out_dir}/${Var_star_date}_dir.tgz.gpg
 					else
 						## Note we are doing some redirection to 'cat' instead of 'echo'ing the line
 						##  as well as prepending the line with '#' commenting hash mark.
--- Script_Helpers/Paranoid_Pipes_Scenario_One.sh
+++ Script_Helpers/Paranoid_Pipes_Scenario_One.sh
@@ -390,7 +390,39 @@ Func_decrypt_file_or_dir(){
 			cat "${_encrypted_path}" | gpg ${Var_gpg_opts} | tar -xf -
 			#Func_message "# ${Var_script_name} running: cat \"${_encrypted_path}\" | gpg ${Var_gpg_opts} | tar -xvf -" '1' '2'
 			#cat "${_encrypted_path}" | gpg ${Var_gpg_opts} | tar -xvf -
-#			_dir_list="$(ls "${_encrypted_path}")"
+			_dir_list="$(ls "${_encrypted_path}")"
+			for _posible_dir in ${_dir_list}; do
+				if [ -d "${_encrypted_path}/${_posible_dir}" ]; then
+					Func_message "# ${Var_script_name} running: ls -hal \"${_encrypted_path}/${_posible_dir}\"" '1' '2'
+					ls -hal "${_encrypted_path}/${_posible_dir}"
+				else
+					Func_message "# ${Var_script_name} reports: not a directory ${_encrypted_path}/${_posible_dir}" '1' '2'
+				fi
+			done
+			Func_message "# ${Var_script_name} running: cd \"${_old_pwd}\"" '1' '2'
+			cd "${_old_pwd}"
+			unset _old_pwd
+		;;
+		*.tgz.gpg)
+			_old_pwd="${PWD}"
+			## Make a directory path under bulk output dir based on
+			##  inputed compressed path name; Bash short-hand tricks
+			_output_dir="${Var_bulk_output_dir}/${_encrypted_path##*/}"
+			_output_dir="${_output_dir%.tgz.gpg*}"
+			## If bulk output directory for compressed & encrypted
+			## directories do not exsist, then mkdir it
+			if ! [ -d "${_output_dir}" ]; then
+				Func_message "# ${Var_script_name} running: mkdir -p \"${_output_dir}\"" '1' '2'
+				mkdir -p "${_output_dir}"
+			fi
+			Func_message "# ${Var_script_name} running: cd \"${_output_dir}\"" '1' '2'
+			cd "${_output_dir}"
+			## Note the trailing dash ('-') with 'tar'
+			Func_message "# ${Var_script_name} running: cat \"${_encrypted_path}\" | gpg ${Var_gpg_opts} | tar -xf -" '1' '2'
+			cat "${_encrypted_path}" | gpg ${Var_gpg_opts} | tar -xf -
+			#Func_message "# ${Var_script_name} running: cat \"${_encrypted_path}\" | gpg ${Var_gpg_opts} | tar -xvf -" '1' '2'
+			#cat "${_encrypted_path}" | gpg ${Var_gpg_opts} | tar -xvf -
+			_dir_list="$(ls "${_encrypted_path}")"
 			for _posible_dir in ${_dir_list}; do
 				if [ -d "${_encrypted_path}/${_posible_dir}" ]; then
 					Func_message "# ${Var_script_name} running: ls -hal \"${_encrypted_path}/${_posible_dir}\"" '1' '2'
