cf7289b5f9629943726d1e64f677e00c015fde69
S0AndS0 <strangerthanbland@gmail.com>
2016-11-30 02:42:20 +0000

    Reverted version two build script to working version
    Also fixed minor bugs within build script for version two tests and added
    second script for getting padding options tested seperatly

--- .travis-ci/version_two_second_tests.sh
+++ .travis-ci/version_two_second_tests.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 export Var_script_dir="${0%/*}"
 export Var_script_name="${0##*/}"
+Var_padding_length='32'
 source "${Var_script_dir}/lib/functions.sh"
 Func_source_file "${Var_script_dir}/lib/variables.sh"
 echo "# ${Var_script_name} started at: $(date -u +%s)"
@@ -14,11 +15,11 @@ if [ -z "${Var_check_path}" ]; then
 fi
 Func_run_sanely "${Var_install_path}/${Var_install_v2_name} --version" "${USER}"
 if [ -e "${Var_install_v2_name}" ]; then
-	${Var_install_v2_name} --debug-level="0" --log-level="7" --enc-yn="yes" --enc-parsing-disown="yes" --enc-copy-save-yn="yes" --enc-copy-save-path="${Var_script_copy_three_name_encrypt}" --enc-copy-save-ownership="${USER}:${USER}" --enc-copy-save-permissions="750" --script-log-path="${Var_encrypt_pipe_three_log}" --enc-pipe-permissions="660" --enc-parsing-output-permissions="660" --enc-parsing-recipient="${Var_gnupg_email}" --enc-parsing-output-rotate-recipient="${Var_gnupg_email}" --enc-pipe-file="${Var_encrypt_pipe_three_location}" --enc-parsing-output-file="${Var_encrypted_three_location}" --enc-parsing-bulk-out-dir="${Var_encrypted_three_bulk_dir}"
+	${Var_install_v2_name} --debug-level="0" --log-level="7" --enc-yn="yes" --enc-parsing-disown="yes" --enc-copy-save-yn="no" --enc-copy-save-path="${Var_script_copy_three_name_encrypt}" --enc-copy-save-ownership="${USER}:${USER}" --enc-copy-save-permissions="750" --script-log-path="${Var_encrypt_pipe_three_log}" --enc-pipe-permissions="660" --enc-parsing-output-permissions="660" --enc-parsing-recipient="${Var_gnupg_email}" --enc-parsing-output-rotate-recipient="${Var_gnupg_email}" --enc-pipe-file="${Var_encrypt_pipe_three_location}" --enc-parsing-output-file="${Var_encrypted_three_location}" --enc-parsing-bulk-out-dir="${Var_encrypted_three_bulk_dir}" --enc-padding-enabled-yn='yes' --enc-padding-length="${Var_padding_length}" --enc-padding-placement='prepend,append'
 	_exit_status=$?
 	Func_check_exit_status "${_exit_status}"
 elif [ -e "${Var_install_path}/${Var_install_v2_name}" ]; then
-	${Var_install_path}/${Var_install_v2_name} --debug-level="6" --log-level="7" --enc-yn="yes" --enc-copy-save-yn="yes" --enc-copy-save-path="${Var_script_copy_three_name_encrypt}" --enc-copy-save-ownership="${USER}:${USER}" --enc-copy-save-permissions="750" --enc-parsing-disown="yes" --script-log-path="${Var_encrypt_pipe_three_log}" --enc-pipe-permissions="660" --enc-parsing-output-permissions="660" --enc-parsing-recipient="${Var_gnupg_email}" --enc-parsing-output-rotate-recipient="${Var_gnupg_email}" --enc-pipe-file="${Var_encrypt_pipe_three_location}" --enc-parsing-output-file="${Var_encrypted_three_location}" --enc-parsing-bulk-out-dir="${Var_encrypted_three_bulk_dir}"
+	${Var_install_path}/${Var_install_v2_name} --debug-level="6" --log-level="7" --enc-yn="yes" --enc-copy-save-yn="no" --enc-copy-save-path="${Var_script_copy_three_name_encrypt}" --enc-copy-save-ownership="${USER}:${USER}" --enc-copy-save-permissions="750" --enc-parsing-disown="yes" --script-log-path="${Var_encrypt_pipe_three_log}" --enc-pipe-permissions="660" --enc-parsing-output-permissions="660" --enc-parsing-recipient="${Var_gnupg_email}" --enc-parsing-output-rotate-recipient="${Var_gnupg_email}" --enc-pipe-file="${Var_encrypt_pipe_three_location}" --enc-parsing-output-file="${Var_encrypted_three_location}" --enc-parsing-bulk-out-dir="${Var_encrypted_three_bulk_dir}" --enc-padding-enabled-yn='yes' --enc-padding-length="${Var_padding_length}" --enc-padding-placement='prepend,append'
 	_exit_status=$?
 	Func_check_exit_status "${_exit_status}"
 else
@@ -103,10 +104,10 @@ else
 	echo "# ${Var_script_name} will cleanup: ${Var_encrypt_pipe_three_location}"
 	rm -v "${Var_encrypt_pipe_three_location}"
 fi
-_background_processes="$(ps aux | grep "${Var_script_copy_three_name_encrypt}" | grep -v grep)"
+_background_processes="$(ps aux | grep "${Var_install_v2_name}" | grep -v grep)"
 if [ "${#_background_processes}" -gt '0' ]; then
 	echo -e "# ${Var_script_name} reports background processes still running:\n#\n$(ps aux | grep "${Var_install_v2_name}" | grep -v grep)\n#"
-	_background_pid="$(ps aux | grep "${Var_script_copy_three_name_encrypt}" | grep -v grep | awk '{print $2}')"
+	_background_pid="$(ps aux | grep "${Var_install_v2_name}" | grep -v grep | awk '{print $2}')"
 	for _pid in ${_background_pid}; do
 		echo "# ${Var_script_name} killing: ${_pid}"
 		kill ${_pid}
@@ -127,25 +128,14 @@ else
 		echo "# ${Var_script_name} reports it not a file: ${Var_encrypted_three_location}"
 	fi
 fi
-if [ -d "${Var_encrypted_three_bulk_dir}" ]; then
-	echo "# ${Var_script_name} running: ls -hal ${Var_encrypted_three_bulk_dir}"
-	ls -hal "${Var_encrypted_three_bulk_dir}"
-	_exit_status=$?
-	Func_check_exit_status "${_exit_status}"
-	echo "# ${Var_script_name} reports: all encryption checks passed"
-else
-	echo "# ${Var_script_name} reports: FAILED to find ${Var_encrypted_three_bulk_dir}"
-	exit 1
-fi
 ## Check decryption within version two of main script processes
-${Var_install_v2_name} --debug-level="0" --log-level="8" --dec-yn="yes" --dec-parsing-disown-yn="no" --dec-bulk-check-sleep="5" --dec-bulk-check-count-max='1' --script-log-path="${Var_decrypt_three_log}" --dec-pass="${Var_pass_location}" --dec-parsing-save-output-yn="yes" --dec-parsing-output-file="${Var_decrypt_raw_three_location}" --enc-parsing-output-file="${Var_encrypted_three_location}" --dec-parsing-bulk-out-dir="${Var_bulk_decryption_three_dir}" --enc-parsing-bulk-out-dir="${Var_encrypted_three_bulk_dir}"
+${Var_install_v2_name} --debug-level="0" --log-level="8" --dec-yn="yes" --dec-parsing-disown-yn="no" --dec-bulk-check-sleep="5" --dec-bulk-check-count-max='1' --script-log-path="${Var_decrypt_three_log}" --dec-pass="${Var_pass_location}" --dec-parsing-save-output-yn="yes" --dec-parsing-output-file="${Var_decrypt_raw_three_location}" --enc-parsing-output-file="${Var_encrypted_three_location}" --dec-parsing-bulk-out-dir="${Var_bulk_decryption_three_dir}" --enc-parsing-bulk-out-dir="${Var_encrypted_three_bulk_dir}"  --enc-padding-enabled-yn='yes' --enc-padding-length="${Var_padding_length}" --enc-padding-placement='prepend,append'
 _exit_status=$?
 Func_check_exit_status "${_exit_status}"
-if [ -r "${Var_decrypt_raw_three_location}" ] && [ -r "${Var_raw_test_three_location}" ] && [ -d "${Var_bulk_decryption_three_dir}" ]; then
+if [ -r "${Var_decrypt_raw_three_location}" ] && [ -r "${Var_raw_test_three_location}" ]; then
 	_decrypted_strings="$(cat "${Var_decrypt_raw_three_location}")"
 	_raw_strings="$(cat "${Var_raw_test_three_location}")"
 	_diff_results="$(diff <(cat "${Var_decrypt_raw_three_location}") <(cat "${Var_raw_test_three_location}"))"
-	_bulk_dec_dir_listing="$(ls "${Var_bulk_decryption_three_dir}")"
 	echo -e "# Contence of decrypted strings #\n${_decrypted_strings}"
 	echo -e "# Contence of un-encrypted strings #\n${_raw_strings}"
 	if [ "${#_diff_results}" != "0" ]; then
@@ -153,16 +143,6 @@ if [ -r "${Var_decrypt_raw_three_location}" ] && [ -r "${Var_raw_test_three_loca
 	else
 		echo "# ${Var_script_name} reports: no differance between strings!"
 	fi
-	for _file in ${_bulk_dec_dir_listing}; do
-		_path="${Var_bulk_decryption_three_dir}/${_file}"
-		if [ -f "${_path}" ]; then
-			echo -e "# ${Var_script_name} reports file detected #\n#${_path}"
-		elif [ -d "${_path}" ]; then
-			echo -e "# ${Var_script_name} reports directory detected #\n#${_path}"
-		else
-			echo "# ${Var_script_name} did not understand path: ${_path}"
-		fi
-	done
 	_background_processes="$(ps aux | grep "${Var_install_v2_name}" | grep -v grep)"
 	if [ "${#_background_processes}" -gt '0' ]; then
 		echo -e "# ${Var_script_name} reports background processes still running:\n#\n$(ps aux | grep "${Var_install_v2_name}" | grep -v grep)\n#"
@@ -184,9 +164,6 @@ elif ! [ -r "${Var_decrypt_raw_three_location}" ]; then
 	exit 1
 elif ! [ -r "${Var_raw_test_three_location}" ]; then
 	echo "# ${Var_script_name} could not read file: ${Var_raw_test_three_location}"
-	exit 1
-elif ! [ -d "${Var_bulk_decryption_three_dir}" ]; then
-	echo "# ${Var_script_name} could not read directory: ${Var_bulk_decryption_three_dir}"
 	exit 1
 else
 	echo "# ${Var_script_name} could not proceed"
