af40ca7d5df5c9bd702608a670c0069face835c2
S0AndS0 <strangerthanbland@gmail.com>
2016-11-30 04:34:35 +0000

    Trying decryption without padding
    This next searies of tests should revial which function is at fault with
    padding/de-padding operations... hoefully.

--- .travis-ci/version_two_second_tests.sh
+++ .travis-ci/version_two_second_tests.sh
@@ -129,7 +129,9 @@ else
 	fi
 fi
 ## Check decryption within version two of main script processes
-${Var_install_v2_name} --debug-level="0" --log-level="8" --dec-yn="yes" --dec-parsing-disown-yn="no" --dec-bulk-check-sleep="5" --dec-bulk-check-count-max='1' --script-log-path="${Var_decrypt_three_log}" --dec-pass="${Var_pass_location}" --dec-parsing-save-output-yn="yes" --dec-parsing-output-file="${Var_decrypt_raw_three_location}" --enc-parsing-output-file="${Var_encrypted_three_location}" --dec-parsing-bulk-out-dir="${Var_bulk_decryption_three_dir}" --enc-parsing-bulk-out-dir="${Var_encrypted_three_bulk_dir}"  --enc-padding-enabled-yn='yes' --enc-padding-length="${Var_padding_length}" --enc-padding-placement='prepend,append'
+${Var_install_v2_name} --debug-level="0" --log-level="8" --dec-yn="yes" --dec-parsing-disown-yn="no" --dec-bulk-check-sleep="5" --dec-bulk-check-count-max='1' --script-log-path="${Var_decrypt_three_log}" --dec-pass="${Var_pass_location}" --dec-parsing-save-output-yn="yes" --dec-parsing-output-file="${Var_decrypt_raw_three_location}" --enc-parsing-output-file="${Var_encrypted_three_location}" --dec-parsing-bulk-out-dir="${Var_bulk_decryption_three_dir}" --enc-parsing-bulk-out-dir="${Var_encrypted_three_bulk_dir}"
+# --enc-padding-enabled-yn='yes' --enc-padding-length="${Var_padding_length}" --enc-padding-placement='prepend,append'
+#${Var_install_v2_name} --debug-level="0" --log-level="8" --dec-yn="yes" --dec-parsing-disown-yn="no" --dec-bulk-check-sleep="5" --dec-bulk-check-count-max='1' --script-log-path="${Var_decrypt_three_log}" --dec-pass="${Var_pass_location}" --dec-parsing-save-output-yn="yes" --dec-parsing-output-file="${Var_decrypt_raw_three_location}" --enc-parsing-output-file="${Var_encrypted_three_location}" --dec-parsing-bulk-out-dir="${Var_bulk_decryption_three_dir}" --enc-parsing-bulk-out-dir="${Var_encrypted_three_bulk_dir}" --enc-padding-enabled-yn='yes' --enc-padding-length="${Var_padding_length}" --enc-padding-placement='prepend,append'
 _exit_status=$?
 Func_check_exit_status "${_exit_status}"
 if [ -r "${Var_decrypt_raw_three_location}" ] && [ -r "${Var_raw_test_three_location}" ]; then
